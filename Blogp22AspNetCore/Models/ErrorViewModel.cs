7d9514bfda9a9d60ab8c33dacfed20899a5d1a3d
Vladislav East <vladislav.sage@yandex.ru>
2025-05-28 15:56:24 +0300

    start

---
+++ Blogp22AspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blogp22AspNetCore.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
